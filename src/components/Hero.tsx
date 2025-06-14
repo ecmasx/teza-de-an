5c394d9d90c6ef8ccb557ca1ffbafe83ae4634b1
Maxim Madan <maximmadan3@gmail.com>
2025-06-14 21:55:32 +0300

    Refactor configuration and update Hero component styles. Migrate from TypeScript to JavaScript for next.config, and enhance Hero section with new text and layout adjustments.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,27 +5,25 @@ import Link from 'next/link'
 export default function Hero() {
   return (
     <section
-      className="relative w-full h-[calc(100vh-250px)] flex flex-col justify-end md:justify-center md:items-start rounded-[0.75rem] overflow-hidden mb-10"
+      className="relative w-full h-[calc(100vh-150px)] flex flex-col justify-end md:justify-center md:items-start rounded-[0.75rem] overflow-hidden mb-10"
       style={{
         backgroundImage: `url('/images/hero.jpg')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
     >
-      <div className="relative z-10 w-full md:max-w-xl text-left text-black drop-shadow-xl p-6 md:p-0 md:mb-0 mb-6 md:ml-0 md:pl-12">
-        <h1 className="text-3xl md:text-5xl font-semibold mb-4 leading-tight">
-          They&apos;re best sellers
-          <br />
-          for a reason
+      <div className="relative z-10 w-full md:max-w-xl text-left text-black drop-shadow-xl p-6 md:p-0 md:mb-0 mb-6 md:ml-0 md:pl-24">
+        <h1 className="text-3xl md:text-4xl font-medium mb-4 leading-tight">
+          Furniture that fits your life.
         </h1>
-        <p className="text-base md:text-lg mb-6 font-medium text-black/80">
-          Try the scents everyone&apos;s talking about.
+        <p className="text-base max-w-md md:text-lg mb-6 font-medium text-black/80">
+          Modern designs. Real-world previews. Zero guesswork.
         </p>
         <Link
           href="/products"
-          className="group w-fit px-6 py-3 bg-black text-white rounded-full text-base font-regular duration-300 transition flex items-center gap-8 hover:bg-white hover:text-black"
+          className="group w-fit px-6 py-3 bg-black text-white rounded-full text-base font-mediumduration-300 transition flex items-center gap-8 hover:bg-white hover:text-black"
         >
-          Shop now
+          Explore Now
           <Icon
             name="arrow"
             size={24}
